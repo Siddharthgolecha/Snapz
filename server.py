656dea227239ea897c820e00eceed35287c82c84
Siddharth Golecha <40235858+Siddharthgolecha@users.noreply.github.com>
2020-06-22 22:34:19 +0530

    Snapz v 1.6
    - Created server.py for upload.
    - File is not uplosded from index.html
    - added folders for server

---
+++ server.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, flash, request, redirect, url_for,session
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+from flask import render_template
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    app.logger.debug(request.method)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            app.logger.debug("1")
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            app.logger.debug("2")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            app.logger.debug("3")
+            filename = secure_filename(file.filename)
+            app.logger.debug(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+            #render_template('index.html',path = "/uploads/"+filename)
+            #return redirect(request.url)
+
+    return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
